43f43a40a290047773624befcdee74d312120531
Code2Craft <cristiangidu01@gmail.com>
2025-04-05 23:59:16 +0300

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,5 +8,14 @@ bot = commands.Bot(command_prefix='!')
 @bot.command()
 async def Hello(ctx):
 		await ctx.send('Hi, the bot is running well!')
-		
+
+@bot.command()
+async def help(ctx):
+    help_text = """
+    **Help Menu**
+    - `!help`: Display this help message.
+    - `!hello`: See if the bot works.
+    """
+    await ctx.send(help_text)
+
 bot.run(TOKEN)
